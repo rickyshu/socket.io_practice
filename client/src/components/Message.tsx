17a20a43fcc99a010551f2fe82dc2cf4b6b4f27f
rickyshu <hoinleekk@naver.com>
2023-08-05 19:43:41 +0900

    design:message 컴포넌트 생성

---
+++ client/src/components/Message.tsx
@@ -0,0 +1,11 @@
+function Message() {
+  // container: width:100%으로 진행한다.
+  return (
+    <div className="container">
+      <input placeholder="메세지를 입력하시오"></input>
+      <button>메세지 전송</button>
+    </div>
+  );
+}
+
+export default Message;
