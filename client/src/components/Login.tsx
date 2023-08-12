34776b1270c6ae0d3446265454919a39b184092a
rickyshu <hoinleekk@naver.com>
2023-08-12 17:36:38 +0900

    feat: 20230812 로그인 useRef 시도

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { socket } from "./Message";
 import { useNavigate } from "react-router-dom";
 
@@ -9,6 +9,7 @@ function Login() {
   const [name, setName] = useState<string>("");
   const [room, setRoom] = useState<string>("");
   const [usersInfo, setUsersInfo] = useState<Users[]>([]);
+  const userInfoRef = useRef([]);
 
   const navigate = useNavigate();
 
@@ -17,16 +18,20 @@ function Login() {
 
     socket.emit("checkforvalidation", room);
 
+    //이걸 여기에서 하면 안 된다.=> 이유: setState를 하게 되면 rerender가 발생하게 되고
+    //문제가 생길 수도 있음 그럼 useRef로 지정하는 게 좋을까? 그럴 수도?
     socket.on("userInfo", (usersInfo) => {
-      console.log(usersInfo, "로그인할 때 나오는 userInfo이다");
-      setUsersInfo(usersInfo);
+      userInfoRef.current.values = usersInfo;
+      console.log(userInfoRef.current.values, "currentValue");
     });
 
     //아래 로직은 벡엔드에서 처리해도 상관없다. 오히려 좋을 수도??
     //아래 방법이 제대로 작동 안 하는 듯? find 말고 다른 방법을 사용해야 하나?
     const alreadyPicked = usersInfo.find((user) => user.name === name);
-    console.log(alreadyPicked, name, room, "alreadypicked");
+    console.log(alreadyPicked, "alreadyPicked");
+    // console.log(usersInfo, "alreadyPicked 밖에");
     if (alreadyPicked) {
+      // console.log(usersInfo, "alreadyPickded 안에");
       return window.alert("이미 있는 이름입니다.");
     }
 
@@ -38,7 +43,6 @@ function Login() {
   };
 
   //dependencies가 뭔가 잘못된 것 같은데 추가로 확인이 필요하다.
-  useEffect(() => {}, [usersInfo]);
   return (
     <form action="" onSubmit={handleSubmitclick}>
       <div className="border-2 border-solid border-slate-950 m-5 flex space-x-4 p-4">
